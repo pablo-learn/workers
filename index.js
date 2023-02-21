5dc8b370ba8337ba76af7abf878ea5f41a9648e9
pabla <pablodelfantexp@gmail.com>
2023-02-21 14:39:18 -0300

    basic changes add statics files

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const { Worker } = require("worker_threads");
+const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const THREAD_COUNT = 12;
+const operationsAmmount = 20_000_000_000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+    res.sendFile("index.html");
+});
 
 app.get("/non-blocking/", (req, res) => {
-    res.status(200).send("This page is non-blocking");
+    res.status(200).send("non blocking page");
 });
 
 function createWorker() {
     return new Promise(function (resolve, reject) {
-        const worker = new Worker("./four_workers.js", {
-            workerData: { thread_count: THREAD_COUNT },
+        const worker = new Worker("./workers.js", {
+            workerData: { threadCount: THREAD_COUNT, operationsAmmount },
         });
         worker.on("message", data => {
             resolve(data);
@@ -29,24 +37,27 @@ app.get("/blocking", async (req, res) => {
     for (let i = 0; i < THREAD_COUNT; i++) {
         workerPromises.push(createWorker());
     }
-    const thread_results = await Promise.all(workerPromises);
+    const threadResutls = await Promise.all(workerPromises);
     const total =
-        thread_results[0] +
-        thread_results[1] +
-        thread_results[2] +
-        thread_results[3];
-    res.status(200).send(`result is ${total}`);
+        threadResutls[0] +
+        threadResutls[1] +
+        threadResutls[2] +
+        threadResutls[3];
+
+    res.status(200).send(
+        `result is ${total}, threadResutls is ${JSON.stringify(threadResutls)}`
+    );
 });
 
 // 22 seg aprox
 app.get("/blocking-main-thread", async (req, res) => {
     let counter = 0;
-    for (let i = 0; i < 20_000_000_000; i++) {
+    for (let i = 0; i < operationsAmmount; i++) {
         counter++;
     }
     res.status(200).send(`result is ${counter}`);
 });
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+    console.log(`App listening on http://localhost:${port}`);
 });
--- workers.js
+++ workers.js
@@ -1,7 +1,11 @@
 const { workerData, parentPort } = require("worker_threads");
 
 let counter = 0;
-for (let i = 0; i < 20_000_000_000 / workerData.thread_count; i++) {
+for (
+    let i = 0;
+    i < workerData.operationsAmmount / workerData.threadCount;
+    i++
+) {
     counter++;
 }
 
